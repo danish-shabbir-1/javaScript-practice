c59d87e29f4860495ac12726416c2f00efedf40b
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-01-05 04:25:19 -0700

    understanding array map method its return new array without edit orignal array

--- Array/script.js
+++ Array/script.js
@@ -1,3 +1,4 @@
+/////////// forEach ////////////
 const  number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 let sum = 0
@@ -7,3 +8,43 @@ number.forEach((item) => {
 });
 
 console.log(sum);
+
+///////// map Function //////////
+
+const str = ['1', '2', '3', '4']
+const convNum = str.map(Number)
+
+console.log('convertArr-->', convNum);
+
+const  product = [
+    {
+        Brand: 'Mercedes',
+        Color: 'White',
+        Model: 2024,
+        Quantity: 10,
+        price: 4000000
+    },
+    {
+        Brand: 'BMW',
+        Color: 'black',
+        Model: 2024,
+        Quantity: 20,
+        price: 3000000
+    },
+    {
+        Brand: 'Bugati',
+        Color: 'blue',
+        Model: 2024,
+        Quantity: 3,
+        price: 400000000
+    }
+]
+
+const newArr = product.map(item => ({
+    Name: item.Brand,
+    Model: item.Model,
+    Price: item.price*item.Quantity
+}))
+
+
+console.log(newArr);
