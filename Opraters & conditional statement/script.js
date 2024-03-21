767cf809f9f561ed98fe5ab8dcc4a19b5ff3acc8
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-03-21 03:56:22 -0700

    learning Arthemetic opraters and Urinary Opraters

---
+++ Opraters & conditional statement/script.js
@@ -0,0 +1,37 @@
+// Opraters in JavaScript 
+
+// 1 Arthmetic Opraters
+// 2 Urinary Opraters
+// 3 Assigment Opraters
+// 4 Comparission Opraters
+// 5 Logical Opraters
+
+
+// 1 Arthmetic Opraters
+
+// let a = 10;
+// let b = 5;
+// let c = 9;
+
+// console.log('a + b = ', a + b); // 15
+// console.log('a - b = ', a - b); // 5
+// console.log('a * b = ', a * b); // 50
+// console.log('a / b = ', a / b); // 2
+// console.log('a ** b = ', a ** b); // 100,000
+// console.log('a % b = ', a % c); // 1
+
+// 2 Urinary Opraters
+
+let a = 10;
+
+// POST Decrement & Increament
+
+// console.log('++ a  = ', ++ a); // 11
+// console.log('-- a  = ', -- a); // 10
+
+// POST Decrement & Increament
+
+// console.log('a --  = ', a --); // 10
+// console.log('a --  = ', a ); // 9
+// console.log('a ++  = ', a ++); // 10
+// console.log('a --  = ', a ); // 11
