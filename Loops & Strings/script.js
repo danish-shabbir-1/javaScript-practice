9c7b4d54c18cc3bc0b53e336f062631dfeee139b
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-04-01 12:13:23 -0700

    for off Loop
    For off loop ko hmm strings or array pr use krte ha unki all value nikale ke lie

--- Loops & Strings/script.js
+++ Loops & Strings/script.js
@@ -1,30 +1,57 @@
+////////////////////////////////////////////////  TOPICS START /////////////////////////////////////////////////////
+
 // 1. For Loop
 // 2. While Loop
 // 3. Do While Loop
+// 4. for off Loop
+// 5. for in Loop
+
+////////////////////////////////////////////////  TOPICS ENDS //////////////////////////////////////////////////////
+
+////////////////////////////////////////////////  NOTES START //////////////////////////////////////////////////////
+
+// 1. While loop or for loop normal use hote ha or do while loop ki condition galat bhi hoto wo 1 bar zarur run krta ha 
+// 2. For off loop ko hmm strings or array pr use krte ha unki all value nikale ke lie 
+// 3. Do while loop ko hmm oject ki key nikalne ke lie use krte ha ye objects pr work krta ha 
+
+////////////////////////////////////////////////  NOTES ENDS ///////////////////////////////////////////////////////
+
+////////////////////////////////////////////////  WORK START ///////////////////////////////////////////////////////
 
 // 1. For Loop
 
-for(let i = 10; i <= 100; i = i+10) {
-    console.log('10', 'x', i/5, '=', i);
-}
+// for(let i = 10; i <= 100; i = i+10) {
+//     console.log('10', 'x', i/5, '=', i);
+// }
 
 // 2. While Loop
 
-let i = 1
+// let i = 1
 
-while (i <= 10) {
-    console.log("Hello Beta =" + i);
-    i++;
-}
+// while (i <= 10) {
+//     console.log("Hello Beta =" + i);
+//     i++;
+// }
 
 // 3. Do While Loop
 
-let j = 20
+// let j = 20
 
-do {
-    console.log("Danish Here");
-    j++
-} while (j <= 10);
+// do {
+//     console.log("Danish Here");
+//     j++
+// } while (j <= 10);
+
+// 4. for off Loop
+
+let str = 'DANISH'
+let arr = ['abc', 'bca', 'dhk']
+
+for(let str1 of obj) {
+  console.log(str1);
+}
+
+////////////////////////////////////////////////  WORK ENDS ///////////////////////////////////////////////////////
 
 
 
@@ -35,4 +62,3 @@ do {
 
 
 
-/// Learn White Loop
